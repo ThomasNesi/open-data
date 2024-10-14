d9cf5ee0320c164d9f497e0bcb968e81f1f12ca3
LENOVO <LENOVO@THOMAS>
2024-10-14 17:38:29 +0200

    Aggiungere i file di progetto.

---
+++ open data/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace open_data
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button_Click = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(62, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(727, 521);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button_Click
+            // 
+            this.button_Click.Location = new System.Drawing.Point(836, 175);
+            this.button_Click.Name = "button_Click";
+            this.button_Click.Size = new System.Drawing.Size(75, 23);
+            this.button_Click.TabIndex = 1;
+            this.button_Click.Text = "mostra";
+            this.button_Click.UseVisualStyleBackColor = true;
+            this.button_Click.Click += new System.EventHandler(this.button_Click_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1086, 564);
+            this.Controls.Add(this.button_Click);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button_Click;
+    }
+}
+
---
+++ open data/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace open_data
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); // Inizializza i componenti
+        }
+
+        private void button_Click_Click_1(object sender, EventArgs e)
+        {
+            // Percorso del file CSV
+            string filePath = "opendata.csv";
+            LoadCsvToDataGridView(filePath);
+        }
+
+        private void LoadCsvToDataGridView(string filePath)
+        {
+            // Crea una tabella per i dati
+            DataTable dataTable = new DataTable();
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                // Leggi la prima riga (intestazioni)
+                string[] headers = reader.ReadLine().Split(',');
+                foreach (string header in headers)
+                {
+                    dataTable.Columns.Add(header); // Aggiungi le colonne alla tabella
+                }
+                while (!reader.EndOfStream)
+                {
+                    // Leggi le righe successive (dati)
+                    string[] rows = reader.ReadLine().Split(',');
+                    dataTable.Rows.Add(rows); // Aggiungi le righe alla tabella
+                }
+            }
+            dataGridView1.DataSource = dataTable; // Mostra la tabella nel DataGridView
+        }
+    }
+}
+
